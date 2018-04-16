5f564e1dc9bb57ee2777c693befddcf02c647232
fantastic-genius <fantastigenius@yahoo.com>
2018-04-16 22:16:57 +0100

    initial-commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Apply for HNG 4.0</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    
+</head>
+<body>
+    <div class="main">
+        <div class="overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="item-right">
+                            <h3>Become the next<br> world class Developer</h3>
+                            <div class="item-content">
+                                <p>
+                                    Are you Ready?
+                                    <input type="button" id="apply" class="btn btn-success" name="apply" value="Apply Now">   
+                                </p>
+                                <p>your time is ticking <?php echo date("h:ia") ?><span> HNG4.0</span></p>
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div>
+                            <img class="circle" src="images/coder.jpg">
+                        </div>                    
+                    </div>                    
+                </div>                
+            </div>
+        </div>
+    </div>
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
